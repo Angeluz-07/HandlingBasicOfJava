a75af96dcd028655ae0ba21be7d173a2ba04ff04
unknown <Alejandro_ramr777@hotmail.com>
2017-10-01 00:17:18 -0500

    Uploading files

---
+++ Switch.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Unit_2;
+
+/**
+ * In this example we'll cover how to use
+ * switch in java. I assumed 
+ * that you've already checked the files:
+ *  PrintAndVariables.java
+ *  UserInput.java
+ *  Arithmetic.java
+ *  IfElse.java
+ * 
+ * As before we'll get user input, so please type 
+ * what the program expects.
+ */ 
+
+public class Switch {
+    public static void main(String[] args) {
+        //still the safe area...
+        /*
+            Sometimes you need to execute a specific
+            piece of code depending on the result
+            of a expression. When the expression is boolean
+            either is true or false you can use if/else statement. 
+            What about if you have more outcomes? for example
+            numbers 1, 2 or 3. Yes you can use:
+                if(number==1){
+                    //code 1
+                }else if(number==2){
+                    //code 2
+                }else if(number==3){
+                    //code 3
+                }else{
+                    //default code in case number no matches 1,2 or 3
+                }
+            good but... if my options are numbers from 1 to 10?            
+            or something
+        choose from a set
+            of choices and executed a code dependi
+            Let's supose you need to choose  set of conditions
+            and just one of them is going to be true
+            and you want a specific instruction
+            in case the follow:
+                if(**condition 1**){
+                    //code 1
+                }else if(**condition 2**){
+                    //code 2
+                }else if(**condition 3**){
+                    //code 3
+                  .
+                  .
+                  .
+                }else if(**condition N**){
+                    //code N
+                }else{
+                    //code in case none condition was true
+                }
+            This normally happens when you have some conditions
+            and you
+        
+        */
+    }
+    
+}
